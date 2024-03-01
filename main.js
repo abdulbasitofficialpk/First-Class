8747241017251068236bca26448be8b38e206eed
abdulbasitofficialpk <127631712+abdulbasitofficialpk@users.noreply.github.com>
2024-03-01 21:33:53 +0500

    TS and JS code

---
+++ main.js
@@ -0,0 +1,21 @@
+//First Class
+console.log("Hello World");
+///--------------///-------------///
+//Second Class
+var myName; //variable declare
+myName = "Abdul Basit"; //variable Initialize
+console.log(myName);
+{
+    var myCountry = "Pakistan"; //Let is used for block scope variable 
+    console.log("I am from ".concat(myCountry));
+}
+{
+    var myNum = 123456789; //Constant, it can't be reassigned but we can change its value
+    console.log(myNum);
+}
+var animal = "Lion"; //TypeScript way to declare a variable with data type 
+console.log("My favorite Animal is ".concat(animal));
+var num = 123456789;
+console.log(num);
+var correct = true;
+console.log(correct);
---
+++ main.ts
@@ -0,0 +1,21 @@
+//First Class
+console.log("Hello World");
+///--------------///-------------///
+//Second Class
+var myName; //variable declare
+myName = "Abdul Basit"; //variable Initialize
+console.log(myName);
+{
+    let myCountry: string = "Pakistan"; //Let is used for block scope variable 
+    console.log(`I am from ${myCountry}`);
+}
+{
+    const myNum: number = 123456789; //Constant, it can't be reassigned but we can change its value
+    console.log(myNum);
+}
+var animal: string = "Lion"; //TypeScript way to declare a variable with data type 
+console.log(`My favorite Animal is ${animal}`);
+var num: number = 123456789;
+console.log(num);
+var correct: boolean = true;
+console.log(correct);
